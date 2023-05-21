3f702b21ba43e8533aa6ab7c0ea15c74af541b78
quydang <81231812+quydang04@users.noreply.github.com>
2023-05-21 17:24:03 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Last modified: 21/05/2023
 
 # Cập nhật thông tin dưới đây
 
@@ -13,7 +14,8 @@ echo "------------------------------------------------------------------------"
 echo "Đang kiểm tra IP, vui lòng đợi trong giây lát!!!!"
 echo "------------------------------------------------------------------------"
 echo ""
-IP=$(curl -s https://api.ipify.org)
+
+IP=$(curl -s https://quydang.name.vn/ip.php)
 ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN" \
   -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
   -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
@@ -28,12 +30,12 @@ RECORD_ID=$(echo "$record_info" | grep -Po '(?<="id":")[^"]*' | head -1)
 OLD_IP=$(echo "$record_info" | grep -Po '(?<="content":")[^"]*')
 
 if [ "$OLD_IP" == "$IP" ]; then
-  echo "$(date): No update needed for $SUBDOMAIN (current IP: $OLD_IP)"
+  echo "$(date): Không cần cập nhật cho $SUBDOMAIN (IP hiện tại: $OLD_IP)"
 else
   update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
     -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
     -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
     -H "Content-Type: application/json" \
     --data '{"type":"A","name":"'$SUBDOMAIN'","content":"'$IP'","ttl":1,"proxied":false}')
-  echo "$(date): Updated DNS record for $SUBDOMAIN from $OLD_IP to $IP"
+  echo "$(date): Đã cập nhật bản ghi DNS cho $SUBDOMAIN từ $OLD_IP thành $IP"
 fi
