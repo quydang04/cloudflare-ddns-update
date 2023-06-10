9d89b242e43caa13613df85f34db083ec91a5bcd
quydang <81231812+quydang04@users.noreply.github.com>
2023-06-10 09:06:02 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
-#Last modified: 21/05/2023
-
+# Last modified: 10/06/2023
 #!/bin/bash
 
 # Cập nhật thông tin dưới đây
@@ -12,41 +11,63 @@ SUBDOMAIN="sub.example.com" # Hãy nhập domain mà bạn muốn cập nhật d
 # Không cần thay đổi phần dưới đây
 
 echo "------------------------------------------------------------------------"
-echo "Đang cập nhật IP, vui lòng đợi trong giây lát!!!!"
+echo "Xin chào bạn đã đến với trình cập nhật DDNS!"
+echo "Vui lòng chọn các tùy chọn bên dưới!"
 echo "------------------------------------------------------------------------"
-echo ""
 
-IP=$(curl -s https://quydang.name.vn/ip.php)
-ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN" \
-  -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
-  -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
-  -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1)
 
-if [ -z "$ZONE_ID" ]; then
-  echo "Không tìm thấy tên miền $DOMAIN trong bản ghi DNS của Cloudflare. Vui lòng kiểm tra lại!"
-  exit 1
-fi
+select option in "Cập nhật DDNS" "Thoát"; do
+  case $option in
+    "Cập nhật DDNS")
+      echo "------------------------------------------------------------------------"
+      echo "Đang cập nhật IP, vui lòng đợi trong giây lát!!!!"
+      echo "------------------------------------------------------------------------"
+      echo ""
+      
+      IP=$(curl -s https://quydang.name.vn/ip.php)
+      ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$DOMAIN" \
+        -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+        -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+        -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1)
 
-record_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$SUBDOMAIN" \
-  -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
-  -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
-  -H "Content-Type: application/json")
+      if [ -z "$ZONE_ID" ]; then
+        echo "Không tìm thấy tên miền $DOMAIN trong bản ghi DNS của Cloudflare. Vui lòng kiểm tra lại!"
+        exit 1
+      fi
 
-RECORD_ID=$(echo "$record_info" | grep -Po '(?<="id":")[^"]*' | head -1)
-OLD_IP=$(echo "$record_info" | grep -Po '(?<="content":")[^"]*')
+      record_info=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=A&name=$SUBDOMAIN" \
+        -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+        -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+        -H "Content-Type: application/json")
 
-if [ -z "$RECORD_ID" ]; then
-  echo "Bản ghi DNS cho tên miền $SUBDOMAIN chưa được thêm hoặc không hợp lệ trên Cloudflare. Vui lòng kiểm tra lại!"
-  exit 1
-fi
+      RECORD_ID=$(echo "$record_info" | grep -Po '(?<="id":")[^"]*' | head -1)
+      OLD_IP=$(echo "$record_info" | grep -Po '(?<="content":")[^"]*')
 
-if [ "$OLD_IP" == "$IP" ]; then
-  echo "$(date +'%d/%m/%Y %H:%M:%S'): Không cần cập nhật cho $SUBDOMAIN (IP hiện tại: $OLD_IP)"
-else
-  update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-    -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
-    -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
-    -H "Content-Type: application/json" \
-    --data '{"type":"A","name":"'$SUBDOMAIN'","content":"'$IP'","ttl":1,"proxied":false}')
-  echo "$(date +'%d/%m/%Y %H:%M:%S'): Đã cập nhật bản ghi DNS cho $SUBDOMAIN từ $OLD_IP thành $IP"
-fi
+      if [ -z "$RECORD_ID" ]; then
+        echo "Bản ghi DNS cho tên miền $SUBDOMAIN chưa được thêm hoặc không hợp lệ trên Cloudflare. Vui lòng kiểm tra lại!"
+        exit 1
+      fi
+
+      if [ "$OLD_IP" == "$IP" ]; then
+        echo "$(date +'%d/%m/%Y %H:%M:%S'): Không cần cập nhật cho $SUBDOMAIN (IP hiện tại: $OLD_IP)"
+        echo "Thoát..."
+        break
+      else
+        update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+          -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+          -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+          -H "Content-Type: application/json" \
+          --data '{"type":"A","name":"'$SUBDOMAIN'","content":"'$IP'","ttl":1,"proxied":false}')
+        echo "$(date +'%d/%m/%Y %H:%M:%S'): Đã cập nhật bản ghi DNS cho $SUBDOMAIN từ $OLD_IP thành $IP"
+        
+        echo "Thoát..."
+        break
+      fi
+      ;;
+    "Thoát")
+      echo "Đã thoát."
+      break
+      ;;
+    *) echo "Tùy chọn không hợp lệ, vui lòng chọn lại.";;
+  esac
+done
